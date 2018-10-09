4f4682e33a807c90eb65e18da0e2693422ad3e73
Ray Donnelly <mingw.android@gmail.com>
2018-10-09 01:56:21 +0100

    Get rid of broken activate scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-export PYTHONHASHSEED=0
-$PYTHON -m pip install . --no-deps --ignore-installed --no-cache-dir -vvv
-
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
-
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/python-libarchive-c-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/python-libarchive-c-deactivate.sh
+PYTHONHASHSEED=0 \
+  python setup.py install --single-version-externally-managed --record record.txt
